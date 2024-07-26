bfb6426774a5cae2c6a2fdf043e89924d40fa779
Chitresh Gyanani <gychitresh1290@gmail.com>
2024-07-26 23:40:37 +0530

    Added hyperparameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,7 +1,7 @@
 import os
 import sys
+import yaml
 from dataclasses import dataclass
-
 from catboost import CatBoostRegressor
 from sklearn.ensemble import (
     AdaBoostRegressor,
@@ -13,7 +13,6 @@ from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
-
 from src.exception import CustomException
 from src.logger import logging
 from src.utils import save_object, evaluate_models
@@ -21,11 +20,19 @@ from src.utils import save_object, evaluate_models
 @dataclass
 class ModelTrainerConfig:
     trained_model_file_path = os.path.join("artifacts", "model.pkl")
-    
+    params_file_path = os.path.join("config", "params.yaml")
+
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
-        
+        self.params = self.load_params()
+
+    def load_params(self):
+        """Load parameters from the YAML file."""
+        with open(self.model_trainer_config.params_file_path, 'r') as file:
+            params = yaml.safe_load(file)
+        return params
+
     def initiate_model_trainer(self, train_array, test_array):
         """
         This method trains multiple regression models on the provided training data, evaluates them
@@ -37,10 +44,10 @@ class ModelTrainer:
         try:
             logging.info("Splitting training and test input data")
             x_train, y_train, x_test, y_test = (
-                train_array[:,:-1],
-                train_array[:,-1],
-                test_array[:,:-1],
-                test_array[:,-1],
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1],
             )
             
             models = {
@@ -50,16 +57,17 @@ class ModelTrainer:
                 "Linear Regression": LinearRegression(),
                 "K-Neighbors Classifier": KNeighborsRegressor(),
                 "XGBClassifier": XGBRegressor(),
-                "CatBoosting Classifier": CatBoostRegressor(verbose = False),
+                "CatBoosting Classifier": CatBoostRegressor(verbose=False),
                 "AdaBoost Classifier": AdaBoostRegressor(),
             }
             
-            model_report:dict = evaluate_models(
-                x_train = x_train, 
-                y_train = y_train, 
-                x_test = x_test, 
-                y_test = y_test, 
-                models = models
+            model_report: dict = evaluate_models(
+                x_train=x_train, 
+                y_train=y_train, 
+                x_test=x_test, 
+                y_test=y_test, 
+                models=models,
+                param=self.params,
             )
             
             best_model_score = max(sorted(model_report.values()))
@@ -71,8 +79,8 @@ class ModelTrainer:
             logging.info(f"Best found model on both training and testing dataset: {best_model}")
             
             save_object(
-                file_path = self.model_trainer_config.trained_model_file_path,
-                obj = best_model,
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model,
             )
             
             predicted = best_model.predict(x_test)
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 import dill
 
@@ -31,7 +32,7 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(x_train, y_train, x_test, y_test, models):
+def evaluate_models(x_train, y_train, x_test, y_test, models, param):
     """
     This function evaluates multiple machine learning models by training them on the
     provided training data and testing them on the test data. It calculates the R2 
@@ -45,8 +46,15 @@ def evaluate_models(x_train, y_train, x_test, y_test, models):
         
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
             
-            model.fit(x_train, y_train)
+            gs = GridSearchCV(model, para, cv=3, n_jobs=3, verbose=1, refit=False)
+            gs.fit(x_train,y_train)
+            
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+            
+            # model.fit(x_train, y_train)
             
             y_train_pred = model.predict(x_train)
             y_test_pred = model.predict(x_test)
